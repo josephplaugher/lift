f29686b85b9d40e1c448230884739c4e24f24045
= <=>
2026-01-03 10:13:48 -0800

    auth is set aside from payments

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), basicSsl()],
   test: {
     globals: true,
     environment: 'jsdom', // required for DOM testing
