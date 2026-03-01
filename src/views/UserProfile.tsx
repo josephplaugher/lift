62057b191e45640eb2549221dbd4d2235ee12a0c
= <=>
2026-03-01 08:36:48 -0800

    wip

--- src/views/UserProfile.tsx
+++ src/views/UserProfile.tsx
@@ -11,10 +11,10 @@ export default function UserProfile({ user, logout, subscribe, paid }: TProfileP
     return (
         <div className='row'>
             <div className="col d-flex flex-column mt-5 justify-content-between align-items-center text-center">
-                <div>Hello {user?.given_name || user?.name}</div>
+                <h1 className='mb-5'>Hello {user?.given_name || user?.name}</h1>
                 {paid == ESubscriptionStatusEnum.Active || paid == ESubscriptionStatusEnum.Trialing ?
                     <>
-                        <p>user stuff here</p>
+                        <p>You are subscribed monthly.</p>
                     </>
                     :
                     <button className="btn btn-success align-self-center mt-5" onClick={() => subscribe()}>subscribe</button>
