3594d65eb0a4623886009ead2acb2838bd1c21c2
= <=>
2026-03-01 08:34:57 -0800

    wip

--- src/views/UnAuthenticated.tsx
+++ src/views/UnAuthenticated.tsx
@@ -3,15 +3,16 @@ import { useAuth0 } from '@auth0/auth0-react';
 export default function UnAuthenticated() {
     const { loginWithRedirect } = useAuth0()
     return (
-        <div className="d-flex justify-content-between align-items-center text-center">
-            <h1>Lift App</h1>
-            <button className="btn btn-primary align-self-center"
+        <div className="d-flex flex-column justify-content-center align-items-center text-center vh-100">
+            <h1><strong>Lift!</strong></h1>
+            <h2 className='my-3'>[logo here]</h2>
+            <button className="btn btn-primary align-self-center my-2 p-3"
                 onClick={() => loginWithRedirect({
                     authorizationParams: {
                         scope: 'openid profile email offline_access'
                     }
                 })}>Sign In</button>
-            <button className="btn btn-primary align-self-center"
+            <button className="btn btn-primary align-self-center p-3"
                 onClick={() => loginWithRedirect({
                     authorizationParams: {
                         screen_hint: "signup",
