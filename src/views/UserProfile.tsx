0be896307e15a8bbce8a6921c7938dda07afbfcb
= <=>
2026-03-12 05:30:24 -0700

    added temp logo. change primary colors

--- src/views/UserProfile.tsx
+++ src/views/UserProfile.tsx
@@ -1,5 +1,6 @@
 import { User } from '@auth0/auth0-react';
 import { ESubscriptionStatusEnum } from '../interfaces/ISubscriptionStatus.enum';
+import barbell from "../images/barbell.webp";
 
 type TProfileParams = {
     user: User;
@@ -10,6 +11,7 @@ type TProfileParams = {
 export default function UserProfile({ user, logout, subscribe, paid }: TProfileParams) {
     return (
         <div className='row'>
+            <div className='d-flex justify-content-end'><img src={barbell} width={"50vw"} /></div>
             <div className="col d-flex flex-column mt-5 justify-content-between align-items-center text-center">
                 <h1 className='mb-5'>Hello {user?.given_name || user?.name}</h1>
                 {paid == ESubscriptionStatusEnum.Active || paid == ESubscriptionStatusEnum.Trialing ?
